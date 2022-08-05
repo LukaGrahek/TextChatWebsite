a361013c445f0a10508bb4f07242a7ee3b2ed3d6
LukaGrahek <lukagrahek3@gmail.com>
2022-08-05 18:56:53 -0400

    previous messages now displayed once logged in

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
 import SignOut from './SignOut'
+import { collection, orderBy, limit ,query , onSnapshot  } from "firebase/firestore"; 
 
 function Chat(){
+    const [messages,setMessages] = useState([])
+    useEffect(()=>{
+        var messagesRef = collection(db, "messages")
+        onSnapshot(query(messagesRef, orderBy('createdAt'), limit(50)),(snapshot)=>{
+            setMessages(snapshot.docs.map(doc=>doc.data()))
+        })
+        //
+    },[])
+    
     return(
         <div>
             <SignOut />
-            signed in!
+            <p>Chat:</p>
+            {/* {console.log(messages.map)} */}
+            {messages.map(({id, text, photoURL})=>(
+                <div key={id}>
+                    
+                    <img src={photoURL} alt=""></img>
+                    <p>{text}</p>
+                </div>
+            ))}
         </div>
     )
-
 }
 
 export default Chat
